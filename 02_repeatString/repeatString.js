9b72e8f340b89ecbcf28e16805b573fc939c158b
hesherkid <somehesherkid@gmail.com>
2024-07-21 15:46:26 -0600

    02_repeatString completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    if (num < 0) {
+        result = "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+        result += string;
+        }
+    }
+    
+    return result;
 };
+
+//repeatString('hello', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
