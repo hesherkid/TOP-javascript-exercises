b88b6bb4dbdb1d6e2973f98a45b2812eb438bd4e
hesherkid <somehesherkid@gmail.com>
2024-07-21 16:36:38 -0600

    07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Ftemp) {
+  let tempCelsius = (Ftemp - 32) * 5 / 9;
+  let roundedTempCelsius = Math.round(tempCelsius *10 ) / 10;
+  return roundedTempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Ctemp) {
+  let tempFahrenheit = (Ctemp * 9 / 5) + 32;
+  let roundedTempFahrenheit = Math.round(tempFahrenheit *10 ) / 10;
+  return roundedTempFahrenheit;
 };
 
 // Do not edit below this line
