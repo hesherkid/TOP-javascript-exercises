4449264aaf913615030ced72e50449df43fde037
hesherkid <somehesherkid@gmail.com>
2024-08-26 18:51:39 -0600

    08_calculator complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((arr, curr) => arr + curr, 0);
+  }
+
+const multiply = function(array) {
+return array.reduce((arr, curr) => arr * curr);
+};
+// you don't set an initial value when using reduce to multiply
+
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1)
+    return 1;
+  for (let i = num -1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
