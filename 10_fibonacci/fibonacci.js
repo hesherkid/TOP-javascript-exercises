40a7d3c975e23ce6becec8f27309103c3dc07e1b
hesherkid <somehesherkid@gmail.com>
2024-08-26 19:28:18 -0600

    10_fibonacci complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let number = Number(num)
+    if (number < 0) {
+        return "OOPS"
+    } else {
+    return number < 1 ? 0
+        : number <= 2 ? 1
+        : fibonacci(number -1) + fibonacci(number - 2);   
+    }
 };
 
 // Do not edit below this line
